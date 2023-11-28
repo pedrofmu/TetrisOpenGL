b9f04ac08404187a792c80745502d7cb4630b8ab
pedrofm3000 <pedrofm@pedrofm.dev>
2023-11-28 13:53:36 +0100

    Instalador

--- instalar.sh
+++ instalar.sh
@@ -20,3 +20,22 @@ else
     echo "OpenGL ya está instalado."
 fi
 
+GLAD_DESTINATION="usr/include/glad/glad.h"  # Cambia esto al directorio donde quieras instalar GLAD
+
+# Verificar si GLAD ya está instalado
+if [ -d "$GLAD_DESTINATION" ]; then
+    echo "GLAD ya está instalado en $GLAD_DESTINATION."
+else
+    echo "Descargando y configurando GLAD..."
+
+    # Crear directorio si no existe
+    mkdir -p "$GLAD_DESTINATION"
+
+    # Descargar GLAD desde el repositorio web
+    wget -O "$GLAD_DESTINATION/glad.zip" "https://glad.dav1d.de/generated/glad.zip"
+    
+    # Descomprimir el archivo descargado
+    unzip "$GLAD_DESTINATION/glad.zip" -d "$GLAD_DESTINATION"
+
+    echo "GLAD instalado correctamente en $GLAD_DESTINATION."
+fi
